d72056226a7cd8fa7bfcdab44f18834a6a23a99a
maverick-farhan <mdfarhanofficial@gmail.com>
2024-01-20 21:55:49 +0530

    Cource source code found here

---
+++ code/introduction.js
@@ -0,0 +1,24 @@
+//Variable Declaration
+
+
+//Variable Definition
+
+
+//Both at same time
+
+
+
+
+//Three ways to declare variables
+
+
+//Hoisting
+
+
+
+//Variable naming rules
+
+
+
+
+//Variable Scopes (Block level, Function level, Global level)
